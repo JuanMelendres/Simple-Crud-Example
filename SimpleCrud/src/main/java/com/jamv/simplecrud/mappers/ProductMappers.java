848145e277c9e2b02b91e9b0c524bb9c8d02b65f
Juan Melendres <juan.mele97@gmail.com>
2025-01-13 15:16:49 -0600

    Update Product Mapper

---
+++ SimpleCrud/src/main/java/com/jamv/simplecrud/mappers/ProductMappers.java
@@ -0,0 +1,29 @@
+package com.jamv.simplecrud.mappers;
+
+import com.jamv.simplecrud.model.Product;
+import com.jamv.simplecrud.model.ProductDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ProductMappers {
+
+    ProductMappers INSTANCE = Mappers.getMapper(ProductMappers.class);
+
+    @Mapping(source = "productID", target = "id")
+    @Mapping(source = "productName", target = "title")
+    @Mapping(source = "productDescription", target = "description")
+    @Mapping(source = "productPrice", target = "price")
+    @Mapping(source = "productStock", target = "stock")
+    @Mapping(source = "productCategory", target = "category")
+    ProductDTO producEntitytToProductDTO(Product product);
+
+    @Mapping(source = "id", target = "productID")
+    @Mapping(source = "title", target = "productName")
+    @Mapping(source = "description", target = "productDescription")
+    @Mapping(source = "price", target = "productPrice")
+    @Mapping(source = "stock", target = "productStock")
+    @Mapping(source = "category", target = "productCategory")
+    Product productDTOToProductEntity(ProductDTO productDTO);
+}
